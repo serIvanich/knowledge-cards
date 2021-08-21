6919d668ccdb8e6052f92b125132909d13a9bb22
marianefedov <marianefedov@gmail.com>
2021-08-21 15:15:47 +0300

    error404 edited

--- src/n1-main/m1-ui/components/error404/Error404.tsx
+++ src/n1-main/m1-ui/components/error404/Error404.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import s from './Error404.module.css';
-import {useSelector} from "react-redux";
-import {AppStateType} from "../../../m2-bll/store";
 
 export const Error404 = () => {
-    const registerError = useSelector<AppStateType, string>(state => state.register.error)
 
     return (
-
         <div className={s.container}>
-            {registerError && <h3 style={{'color':'#fff'}}>{registerError}</h3>}
             <div className={s.rail}>
                 <div className={s.stamp + " " + s.four}>4</div>
                 <div className={s.stamp + " " + s.zero}>0</div>
