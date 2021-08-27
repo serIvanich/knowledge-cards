01bd3325842529e8699f3651a03e08eafdb35785
Tanya Kravchenko <mail-tanja@mail.ru>
2021-08-27 18:19:49 +0300

    deleted forgot from passwordRecovery-api.ts

--- src/n1-main/m3-dall/passwordRecovery-api.ts
+++ src/n1-main/m3-dall/passwordRecovery-api.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import {routes} from '../m1-ui/routes/routes';
 
 export const instance = axios.create({
     baseURL: 'https://neko-back.herokuapp.com/2.0/',
@@ -11,18 +10,7 @@ export type ForgotDataType = {
 }
 
 export const passwordRecoveryApi = {
-    forgot: async (email: string) => {
-        return await instance.post<ForgotDataType>('auth/forgot', {
-            email,
-            from: 'test-front-admin <mail-tanja@mail.ru>',
-            message: `<div style="background-color: lime; padding: 15px">
-                    password recovery link: 
-                        <a href='http://localhost:3000/knowledge-cards#${routes.setPass}/$token$'>link</a>
-                    </div>
-                 `
-        })
-    },
-    setNewPassword (password:string, token:string) {
+    setNewPassword(password: string, token: string) {
         return instance.post('/auth/set-new-password', {
             password,
             resetPasswordToken: token,
