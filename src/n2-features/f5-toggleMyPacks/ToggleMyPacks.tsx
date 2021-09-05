4f762c13e3350a9321a4220f577d4e4bc74cf271
Tanya Kravchenko <mail-tanja@mail.ru>
2021-09-05 14:39:56 +0300

    a little refactoring

--- src/n2-features/f5-toggleMyPacks/ToggleMyPacks.tsx
+++ src/n2-features/f5-toggleMyPacks/ToggleMyPacks.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import {useDispatch, useSelector} from 'react-redux';
 import {setMyPacksAC} from '../../n1-main/m2-bll/reducers/packs-reducer';
 import s from '../../n1-main/m1-ui/components/profile/Profile.module.scss'
-import {AppStateType} from "../../n1-main/m2-bll/store";
+import {AppStateType} from '../../n1-main/m2-bll/store';
 
 export const ToggleMyPacks: React.FC = () => {
-const myPacks = useSelector<AppStateType, boolean>(state => state.packs.myPacks)
+    const myPacks = useSelector<AppStateType, boolean>(state => state.packs.myPacks)
     const dispatch = useDispatch()
     const showAllPacks = () => {
         dispatch(setMyPacksAC(false))
@@ -15,14 +15,12 @@ const myPacks = useSelector<AppStateType, boolean>(state => state.packs.myPacks)
         dispatch(setMyPacksAC(true))
     }
 
-
     return (
         <div className={s.buttonMyAll}>
             Show packs cards
 
-            <button className={myPacks? s.buttonMyAllIsOn: ''} onClick={showMyPacks}>My</button>
-            <button className={!myPacks? s.buttonMyAllIsOn: ''} onClick={showAllPacks}>All</button>
-
+            <button className={myPacks ? s.buttonMyAllIsOn : ''} onClick={showMyPacks}>My</button>
+            <button className={!myPacks ? s.buttonMyAllIsOn : ''} onClick={showAllPacks}>All</button>
         </div>
     )
 }
